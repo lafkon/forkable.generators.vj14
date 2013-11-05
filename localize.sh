278dd1210fd5bb144ecccdf40b87bfb50d592234
christop <christoph@lafkon.net>
2013-11-05 11:28:26 +0100

    now skip existing

--- localize.sh
+++ localize.sh
@@ -9,7 +9,19 @@
                 rev`
 
       cd $FILEPATH
-      wget `cat \`basename $DWNLD\` | shuf -n 1`
+
+       DWNLDURL=`cat \`basename $DWNLD\` | shuf -n 1`
+      DWNLDFILE=`echo $DWNLDURL | \
+                 rev | \
+                 cut -d "/" -f 1 | \
+                 rev`
+
+      if [ ! -f $DWNLDFILE ]; then
+           wget $DWNLDURL
+      else
+           echo $DWNLDFILE "already here"
+      fi
+
       cd -
 
  done
