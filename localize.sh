a353e645436e049737f37108cd3625a4ea18e629
christop <christoph@lafkon.net>
2013-11-25 13:14:52 +0100

    check if file is online, if not try different

--- localize.sh
+++ localize.sh
@@ -10,19 +10,30 @@
 
       cd $FILEPATH
 
-       DWNLDURL=`cat \`basename $DWNLD\` | shuf -n 1`
+       DWNLDURL=`cat \`basename $DWNLD\` | \
+                 shuf -n 1`
       DWNLDFILE=`echo $DWNLDURL | \
                  rev | \
                  cut -d "/" -f 1 | \
                  rev`
 
       if [ ! -f $DWNLDFILE ]; then
-           wget $DWNLDURL
+
+       while [ `curl --silent --head "$DWNLDURL" | \
+                grep HTTP | grep 200 | wc -l` -lt 1 ];
+        do
+            echo "$DWNLDURL does not exist"
+            DWNLDURL=`cat \`basename $DWNLD\` | \
+                      shuf -n 1`
+       done
+
+            wget $DWNLDURL
+
       else
            echo $DWNLDFILE "already here"
       fi
 
-      cd -
+      cd - > /dev/null
 
  done
 
